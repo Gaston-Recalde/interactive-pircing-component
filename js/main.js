ca2091d0fe5e4d4b8b6260347ea952c9a4e2ce1c
Gaston Recalde <gastonrg94@gmail.com>
2023-03-20 09:33:10 -0300

    Proyecto terminado

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// const toggle = document.querySelector('.toggle');
-// const toggleBtn = document.querySelector('.toggle-btn');
-// const monthYear = document.querySelector('.card__price');
-// const price = document.querySelector('.price');
-
-// toggle.addEventListener('click', () => {
-//     if(!toggleBtn.classList.contains('toggle-btn-active')){
-//         toggleBtn.classList.add('toggle-btn-active');
-//         monthYear.innerHTML = ' / year';
-//         price.innerHTML = '$144.00';
-//     } else {
-//         toggleBtn.classList.remove('toggle-btn-active');
-//         monthYear.innerHTML = ' / month';
-//         price.innerHTML = '$16.00';
-//     }
-// });
-
 const inputRange = document.querySelector('.slider') 
 const price = document.querySelector('.price');
 const toggle = document.querySelector('.toggle');
